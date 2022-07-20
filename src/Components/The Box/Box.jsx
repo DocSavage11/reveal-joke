71d42bc042ee25e51e4f7f1f3c07559ce9a643e8
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-07-20 14:32:57 -0500

    Animation works

--- src/Components/The Box/Box.jsx
+++ src/Components/The Box/Box.jsx
@@ -1,22 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 import './Box.css'
 
 const Box = () => {
+
+    const [doors, setDoors] = useState(false);
+
+    const openDoors = () => {
+        setDoors((prevState) => !prevState)
+    }
+
   return (
     <>
     <div className="Container">
-        <div className="Outer-Door-Left"></div>
+        <div className={doors ? "Outer-Door-Left Left-Door " : "Outer-Door-Left"}></div>
 
-        <div className="Inner-Door-Top"></div>
+        <div className={doors ? "Inner-Door-Top Top-Door " : "Inner-Door-Top"}></div>
 
 
 
-        <div className="Inner-Door-Bottom"></div>
+        <div className={doors ? "Inner-Door-Bottom Bottom-Door " : "Inner-Door-Bottom"}></div>
 
-        <div className="Outer-Door-Right"></div>
+        <div className={doors ? "Outer-Door-Right Right-Door " : "Outer-Door-Right"}></div>
     </div>
 
-    <button className='Revealer Reveal-Button'>Open Sesame</button>
+    <button onClick={openDoors} className='Revealer Reveal-Button'>Open Sesame</button>
     <button className='Revealer Hide-Button'>Close Sesame</button>
     </>
   )
